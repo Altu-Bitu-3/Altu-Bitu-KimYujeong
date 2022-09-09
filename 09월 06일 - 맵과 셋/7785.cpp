4b065840a322f56173b19fb4aefbfee3f50fcf39
kimyu0218 <serena35@ewhain.net>
2022-09-09 17:09:09 +0900

    [맵과 셋] 9월 9일 - Update

--- 09월 06일 - 맵과 셋/7785.cpp
+++ 09월 06일 - 맵과 셋/7785.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <map>
+#include <set>
 
 using namespace std;
 
@@ -7,17 +7,17 @@ int main() {
     int n;
     cin >> n;
 
-    map<string, bool> m; // key : 직원 이름, value : 회사에 있는지 여부
+    set<string> s; // 회사에 있는 직원 저장하는 set
     string name, log;
     while(n--) {
         cin >> name >> log;
-        if(log == "enter") m[name] = true; // 출근 == 회사에 있음
-        if(log == "leave") m[name] = false; // 퇴근 == 회사에 없음
+        if(log == "enter") s.insert(name); // 출근 == 회사에 있음
+        if(log == "leave") s.erase(name); // 퇴근 == 회사에 없음
     }
 
-    // (map은 오름차순 정렬 -> 내림차순으로 출력하기 위해서는 end부터 탐색)
-    for(auto iter = --m.end(); iter != --m.begin(); iter--) { // 회사에 있는 사람 출력
-        if(m[iter->first]) cout << iter->first << '\n';
+    // (set은 오름차순 정렬 -> 내림차순으로 출력하기 위해서는 end부터 탐색)
+    for(auto iter = s.rbegin(); iter != s.rend(); iter++) { // 회사에 있는 사람 출력
+        cout << *iter << '\n';
     }
     return 0;
 }
