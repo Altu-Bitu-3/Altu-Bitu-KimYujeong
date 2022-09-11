5d1e5b1a46eae9b86c50095995f7cc03b51bd196
kimyu0218 <serena35@ewhain.net>
2022-09-11 17:35:14 +0900

    [맵과 셋] 9월 10일 - Update

--- 09월 06일 - 맵과 셋/10757.cpp
+++ 09월 06일 - 맵과 셋/10757.cpp
@@ -6,36 +6,16 @@ void addBigNum(string a, string b) { // 큰 수의 덧셈
     string rev_ans = ""; // 덧셈 결과 거꾸로 저장
     int p1 = a.length()-1, p2 = b.length()-1; // 각각 a와 b의 자릿수를 가리키는 포인터
 
-    bool ten = false; // 올림 여부 표시
+    int ten = false; // 올림 여부 표시
     while(p1 >= 0 && p2 >= 0) {
-        int digit = (a[p1] - '0') + (b[p2] - '0') + ten;
-        ten = false;
-        if(digit >= 10) { // 올림 필요한 경우
-            ten = true;
-            digit -= 10;
-        }
-        rev_ans += to_string(digit);
-        p1--; p2--;
+        int digit = (a[p1--] - '0') + (b[p2--] - '0') + ten;
+        ten = digit/10;
+        rev_ans += to_string(digit % 10);
     }
     while(p1 >= 0) { // p1의 자릿수가 남아있는 경우
-        int digit = (a[p1] - '0') + ten;
-        ten = false;
-        if(digit >= 10) { // 올림 필요한 경우
-            ten = true;
-            digit -= 10;
-        }
-        rev_ans += to_string(digit);
-        p1--;
-    }
-    while(p2 >= 0) { // p2의 자릿수가 남아있는 경우
-        int digit = (b[p2] - '0') + ten;
-        ten = false;
-        if(digit >= 10) { // 올림 필요한 경우
-            ten = true;
-            digit -= 10;
-        }
-        rev_ans += to_string(digit);
-        p2--;
+        int digit = (a[p1--] - '0') + ten;
+        ten = digit/10;
+        rev_ans += to_string(digit % 10);
     }
     if(ten) rev_ans += '1'; // 올림이 남아있는 경우 1 추가
 
@@ -47,6 +27,8 @@ int main() {
     string a, b;
     cin >> a >> b;
 
-    addBigNum(a, b);
+    // (자릿수가 큰 수를 첫번째 인자로, 자릿수가 작은 수를 두번째 인자로 넘김)
+    if(a.length() > b.length()) addBigNum(a, b);
+    else addBigNum(b, a);
     return 0;
 }
