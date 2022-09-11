7333e78df58d6f32db91fffb8186f47ada3744c7
kimyu0218 <serena35@ewhain.net>
2022-09-11 17:41:05 +0900

    [맵과 셋] 9월 10일 - Update

--- 09월 06일 - 맵과 셋/10757.cpp
+++ 09월 06일 - 맵과 셋/10757.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void addBigNum(string a, string b) { // 큰 수의 덧셈
+string addBigNum(string a, string b) { // 큰 수의 덧셈
     string rev_ans = ""; // 덧셈 결과 거꾸로 저장
     int p1 = a.length()-1, p2 = b.length()-1; // 각각 a와 b의 자릿수를 가리키는 포인터
 
@@ -19,8 +19,10 @@ void addBigNum(string a, string b) { // 큰 수의 덧셈
     }
     if(ten) rev_ans += '1'; // 올림이 남아있는 경우 1 추가
 
+    string answer = "";
     for(int i = rev_ans.length()-1; i >= 0; i--) // 거꾸로 된 답안 바르게 출력
-        cout << rev_ans[i];
+        answer.push_back(rev_ans[i]);
+    return answer;
 }
 
 int main() {
@@ -28,7 +30,7 @@ int main() {
     cin >> a >> b;
 
     // (자릿수가 큰 수를 첫번째 인자로, 자릿수가 작은 수를 두번째 인자로 넘김)
-    if(a.length() > b.length()) addBigNum(a, b);
-    else addBigNum(b, a);
+    if(a.length() > b.length()) cout << addBigNum(a, b);
+    else cout << addBigNum(b, a);
     return 0;
 }
