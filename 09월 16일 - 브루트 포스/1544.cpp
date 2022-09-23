7c416c1b0babb394817070e5a6caea13f57df2ea
kimyu0218 <serena35@ewhain.net>
2022-09-23 23:50:35 +0900

    [브루트 포스] 9월 23일 - Update

--- 09월 16일 - 브루트 포스/1544.cpp
+++ 09월 16일 - 브루트 포스/1544.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <deque>
+#include <string>
 #include <set>
 
 using namespace std;
@@ -9,19 +9,13 @@ bool isCycleWord(string s1, string s2) { // s1과 s2는 같은 단어인가
     if(s1.length() != s2.length()) return false; // 길이가 같지 않은 경우, 사이클 단어 아님
 
     int len = s1.length();
-    deque<char> dq; // s2 문자열 덱 만들기
     for(int i = 0; i < len; i++) {
-        dq.push_back(s2[i]);
-    }
+        if(s1 == s2) return true; // 사이클 문자 (== 같은 문자)인 경우
 
-    for(int i = 0; i < len; i++) {
-        string cmp = "";
-        for(auto iter = dq.begin(); iter != dq.end(); iter++) cmp += *iter;
-        if(s1 == cmp) return true; // 사이클 문자 (== 같은 문자)인 경우
-
-        // 맨 처음에 위치한 문자를 가장 뒤로 보내기 (시계방향 회전)
-        char front = dq.front(); dq.pop_front();
-        dq.push_back(front);
+        // 0번 인덱스의 문자 지우고 문자열 맨 뒤에 push
+        char front = s2.front();
+        s2 = s2.erase(0, 1);
+        s2.push_back(front);
     }
     return false;
 }
