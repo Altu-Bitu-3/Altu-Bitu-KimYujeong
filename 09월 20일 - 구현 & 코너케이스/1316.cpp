78c8ec5e5697d4fe87929d1f538b460a63aac79d
kimyu0218 <serena35@ewhain.net>
2022-09-26 22:42:55 +0900

    [구현 & 코너케이스] 9월 26일 - Update

--- 09월 20일 - 구현 & 코너케이스/1316.cpp
+++ 09월 20일 - 구현 & 코너케이스/1316.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
-#include <map>
+#include <set>
 
 using namespace std;
 
 bool isGroupWord(string word) {
-    map<char, int> m; // (key: 알파벳, value: 빈도)
-    char pre = word[0]; m[pre]++;
+    set<char> s;
+    char pre = word[0]; s.insert(word[0]);
     for(int i = 1; i < word.size(); i++) {
-        if(word[i] == pre) m[word[i]]++; // 연속해서 같은 문자가 나온 경우
-        else if(word[i] != pre && m[word[i]]) return false; // word[i]가 떨어져서 나타난 경우
-        else { // 새로운 다른 문자가 나온 경우
+        if(s.find(word[i]) == s.end()) { // 새로운 다른 문자가 나온 경우
             pre = word[i];
-            m[word[i]]++;
+            s.insert(word[i]); // set에 새로운 문자 추가
         }
+        else if(word[i] != pre) return false; // set에 존재하는 문자가 떨어져서 나타난 경우
     }
     return true;
 }
--- 09월 20일 - 구현 & 코너케이스/2840.cpp
+++ 09월 20일 - 구현 & 코너케이스/2840.cpp
@@ -5,33 +5,44 @@ using namespace std;
 
 const int SIZE = 26;
 
-int main() {
-    int pos[SIZE]; // 알파벳의 위치 (-1 : 바퀴에 해당 알파벳이 존재하지 않음)
-    for(int i = 0; i < SIZE; i++) pos[i] = -1;
+// 화살표가 가리키는 위치에 이미 다른 알파벳이 있는가
+bool anotherAlphabet(int pointer, char alphabet, vector<char> wheel) {
+    if(wheel[pointer] != '?' && wheel[pointer] != alphabet) return true;
+    return false;
+}
 
+// 해당 알파벳이 현재 알파벳이 가리키는 곳 외에 다른 위치에도 존재하는가
+bool duplicate(int pointer, char alphabet, vector<int> pos) {
+    if(pos[alphabet-'A'] != -1 && pos[alphabet-'A'] != pointer) return true;
+    return false;
+}
+
+bool isLucky(int pointer, char alphabet, vector<char> wheel, vector<int> pos) { // 행운의 바퀴인가
+    // 1. 화살표가 가리키는 위치에 이미 다른 알파벳이 있는 경우
+    if(anotherAlphabet(pointer, alphabet, wheel)) return false;
+    // 2. 해당 알파벳이 현재 알파벳이 가리키는 곳 외에 다른 위치에도 존재하는 경우
+    if(duplicate(pointer, alphabet, pos)) return false;
+    return true;
+}
+
+int main() {
     int n, k, s;
     char alphabet;
     cin >> n >> k;
 
     int pointer = 0; // 화살표가 가리키는 알파벳 위치
     bool lucky = true; // 행운의 바퀴 여부
+    vector<int> pos (SIZE, -1); // 알파벳의 위치 (-1 : 바퀴에 해당 알파벳이 존재하지 않음)
     vector<char> wheel (n, '?');
     while(k--) { // k번 회전
         cin >> s >> alphabet;
         pointer = (pointer - (s % n) + n) % n; // 시계방향 회전
 
         // 행운의 바퀴가 없는 경우
-        // 1. 화살표가 가리키는 위치에 이미 다른 알파벳이 있는 경우
-        if(wheel[pointer] != '?' && wheel[pointer] != alphabet) {
+        if(!lucky || !isLucky(pointer, alphabet, wheel, pos)) {
             lucky = false;
             continue;
         }
-        // 2. 해당 알파벳이 현재 알파벳이 가리키는 곳 외에 다른 위치에도 존재하는 경우
-        if(pos[alphabet-'A'] != -1 && pos[alphabet-'A'] != pointer) {
-            lucky = false;
-            continue;
-        }
-
         wheel[pointer] = alphabet;
         pos[alphabet-'A'] = pointer;
     }
