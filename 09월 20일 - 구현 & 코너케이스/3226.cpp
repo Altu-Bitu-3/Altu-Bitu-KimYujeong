ff6500099c6e3885e061319b279352c2ebec5de4
kimyu0218 <serena35@ewhain.net>
2022-09-28 00:40:23 +0900

    [구현 & 코너케이스] 9월 28일 - Update

--- 09월 20일 - 구현 & 코너케이스/3226.cpp
+++ 09월 20일 - 구현 & 코너케이스/3226.cpp
@@ -2,35 +2,27 @@
 
 using namespace std;
 
-typedef pair<int, int> pi;
-
-pi endTime(int hh, int mm, int dd) { // 전화가 끝나는 시간
-    mm += dd;
-    if(mm >= 60) {
-        mm -= 60;
-        hh += 1;
-    }
-    if(hh >= 24) hh -= 24;
-    return {hh, mm};
-}
-
 int computeCost(string start, int dd) { // 전화 요금 계산
     int hh = stoi(start.substr(0, 2));
     int mm = stoi(start.substr(3, 2));
 
-    pi end = endTime(hh, mm, dd);
-    if(hh == 6 && end.first == 7) { // 전화 요금이 달라지는 시점 1
-        int cost1 = end.second * 10;
+    // 전화 끝나는 시간
+    int end_mm = mm + dd;
+    int end_hh = hh + (end_mm/60);
+    end_mm %= 60; end_hh %= 24;
+
+    if(hh == 6 && end_hh == 7) { // 전화 요금이 달라지는 시점 1
+        int cost1 = end_mm * 10;
         int cost2 = (60 - mm) * 5;
         return cost1 + cost2;
     }
-    else if(hh == 18 && end.first == 19) { // 전화 요금이 달라지는 시점 2
-        int cost1 = end.second * 5;
+    if(hh == 18 && end_hh == 19) { // 전화 요금이 달라지는 시점 2
+        int cost1 = end_mm * 5;
         int cost2 = (60 - mm) * 10;
         return cost1 + cost2;
     }
-    else if(end.first < 7 || end.first >= 19) return dd * 5;
-    else return dd * 10;
+    if(end_hh < 7 || end_hh >= 19) return dd * 5;
+    return dd * 10;
 }
 
 int main() {
--- 09월 20일 - 구현 & 코너케이스/8911.cpp
+++ 09월 20일 - 구현 & 코너케이스/8911.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-const int MAX = 500, MIN = -500;
 int dx[4] = {0, -1, 0, 1}, dy[4] = {-1, 0, 1, 0}; // 상 좌 하 우
 
 void squareArea(int row, int col, int &left, int &right, int &top, int &bottom) {
@@ -11,7 +10,7 @@ void squareArea(int row, int col, int &left, int &right, int &top, int &bottom) 
 }
 
 int moveTurtle(string cmd) { // 거북이 이동시키기
-    int left = MAX, right = MIN, top = MIN, bottom = MAX;
+    int left = 1, right = -1, top = -1, bottom = 1;
 
     int row = 0, col = 0, dir = 0; // 시작 위치 및 초기 방향
     squareArea(row, col, left, right, top, bottom); // 사각형 영역 갱신
@@ -20,13 +19,11 @@ int moveTurtle(string cmd) { // 거북이 이동시키기
         switch(cmd[i]) {
             case 'F': // 한 눈금 앞으로 : 현재 방향에서 앞으로 한 칸
                 row += dy[dir]; col += dx[dir];
-                squareArea(row, col, left, right, top, bottom); // (이동시켰으니 사각형 영역 갱신)
                 break;
             case 'B': { // 한 눈금 뒤로 : 현재 방향과 반대로 앞으로 한 칸 (방향은 전환하지 않음)
                 int back = (dir + 2) % 4;
                 row += dy[back];
                 col += dx[back];
-                squareArea(row, col, left, right, top, bottom); // (이동시켰으니 사각형 영역 갱신)
                 break;
             }
             case 'L': // 왼쪽으로 90도 회전
@@ -34,6 +31,7 @@ int moveTurtle(string cmd) { // 거북이 이동시키기
             case 'R': // 오른쪽으로 90도 회전
                 dir = (dir + 3) % 4; break;
         }
+        squareArea(row, col, left, right, top, bottom); // (이동시켰으니 사각형 영역 갱신)
     }
     return (right - left) * (top - bottom);
 }
