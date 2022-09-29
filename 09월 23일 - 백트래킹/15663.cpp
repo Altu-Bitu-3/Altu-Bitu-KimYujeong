1e00c55f48a13ebf1b53ce3230649b5b9574ffa4
kimyu0218 <serena35@ewhain.net>
2022-09-30 02:36:26 +0900

    [백트래킹] 9월 30일 - Update

--- 09월 23일 - 백트래킹/15663.cpp
+++ 09월 23일 - 백트래킹/15663.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-const int SIZE = 8, MAX = 10000;
+const int SIZE = 8;
 int arr[SIZE]; // 수열
-int check[MAX+1]; // check[i] : i 등장 횟수
 
-void backtracking(int cnt, int m) {
+void backtracking(int cnt, int m, int n, vector<int> num, vector<bool> visited) {
     if(cnt == m) { // 길이가 m인 수열 출력
         for(int i = 0; i < m; i++)
             cout << arr[i] << ' ';
@@ -14,25 +15,30 @@ void backtracking(int cnt, int m) {
         return;
     }
 
-    for(int i = 1; i <= MAX; i++) {
-        if(check[i]) {
-            check[i]--;
-            arr[cnt] = i;
-            backtracking(cnt+1, m);
-            check[i]++;
-        }
+    int pre = 0;
+    for(int i = 0; i < n; i++) {
+        // 이미 탐색한 수열이거나 이미 사용한 숫자 pass
+        if(pre == num[i] || visited[i]) continue;
+
+        arr[cnt] = num[i];
+        visited[i] = true; // (i번째 수 사용)
+        backtracking(cnt+1, m, n, num, visited);
+        visited[i] = false; // (i번째 수 사용 취소)
+        pre = num[i];
     }
 }
 
 int main() {
-    int n, m, num;
+    int n, m;
     cin >> n >> m;
 
-    while(n--) { // n개의 수 입력
-        cin >> num;
-        check[num]++;
+    vector<int> num(n, 0);
+    vector<bool> visited(n, false); // 방문 여부 저장
+    for(int i = 0; i < n; i++) { // n개의 수 입력
+        cin >> num[i];
     }
+    sort(num.begin(), num.end()); // 오름차순 정렬
 
-    backtracking(0, m);
+    backtracking(0, m, n, num, visited);
     return 0;
 }
--- 09월 23일 - 백트래킹/2580.cpp
+++ 09월 23일 - 백트래킹/2580.cpp
@@ -20,21 +20,24 @@ void backtracking(int idx) {
     }
 
     int row = idx / SIZE, col = idx % SIZE;
-    if(board[row][col]) return backtracking(idx+1); // 이미 숫자가 있는 경우 pass
+    if(board[row][col]) { // 이미 숫자가 있는 경우 pass
+        return backtracking(idx + 1);
+    }
 
     for(int i = 1; i <= SIZE; i++) { // i : 대입할 숫자 (1 - 9)
-        if(check_row[row][i] || check_col[col][i] || check_square[squareNum(row, col)][i]) continue;
+        int square = squareNum(row, col);
+        if(check_row[row][i] || check_col[col][i] || check_square[square][i]) continue;
         check_row[row][i] = true;
         check_col[col][i] = true;
-        check_square[squareNum(row, col)][i] = true;
+        check_square[square][i] = true;
         board[row][col] = i; // (row, col)에 i 집어넣기
 
         backtracking(idx+1);
-        if(complete) return; // 스도쿠 완성한 경우 종료
+        if(complete) { return; } // 스도쿠 완성한 경우 종료
 
         check_row[row][i] = false;
         check_col[col][i] = false;
-        check_square[squareNum(row, col)][i] = false;
+        check_square[square][i] = false;
         board[row][col] = 0;
     }
 }
@@ -43,7 +46,7 @@ int main() {
     for(int i = 0; i < SIZE; i++) {
         for(int j = 0; j < SIZE; j++) {
             cin >> board[i][j];
-            if(!board[i][j]) continue;
+            if(!board[i][j]) { continue; }
             check_row[i][board[i][j]] = true;
             check_col[j][board[i][j]] = true;
             check_square[squareNum(i, j)][board[i][j]] = true;
@@ -52,8 +55,9 @@ int main() {
 
     backtracking(0);
     for(int i = 0; i < SIZE; i++) { // 완성한 스도쿠 출력
-        for(int j = 0; j < SIZE; j++)
+        for(int j = 0; j < SIZE; j++) {
             cout << board[i][j] << ' ';
+        }
         cout << '\n';
     }
     return 0;
