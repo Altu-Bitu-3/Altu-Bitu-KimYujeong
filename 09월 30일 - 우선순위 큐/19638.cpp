cb97abe275e414ea3086e05babb6488e09eef1bb
kimyu0218 <serena35@ewhain.net>
2022-10-06 23:41:42 +0900

    [우선순위 큐] 10월 6일 - Update

--- 09월 30일 - 우선순위 큐/19638.cpp
+++ 09월 30일 - 우선순위 큐/19638.cpp
@@ -5,22 +5,23 @@ using namespace std;
 
 typedef priority_queue<int> pq;
 
-void magicHammer(int h_centi, int t, pq height) {
+string magicHammer(int h_centi, int t, pq height) {
     int cnt = 0; // 뿅망치 사용횟수 저장
     while (cnt < t) {
-        if(h_centi > height.top() || height.top() == 1) break; // 뿅망치 사용 중단
+        int tallest = height.top();
+        if(h_centi > tallest || tallest == 1) break; // 뿅망치 사용 중단
 
-        height.push(height.top()/2);
+        height.push(tallest/2);
         height.pop();
         cnt++;
     }
 
     int tallest_giant = height.top(); // 현재 가장 큰 거인의 키
     if(h_centi > tallest_giant) { // 1. 모든 거인이 센티보다 작은 경우
-        cout << "YES\n" <<  cnt;
+       return "YES\n" + to_string(cnt);
     }
     else { // 2. 센티보다 큰 거인이 있는 경우
-        cout << "NO\n" << tallest_giant;
+        return ("NO\n" + to_string(tallest_giant));
     }
 }
 
@@ -34,6 +35,6 @@ int main() {
         height.push(h);
     }
 
-    magicHammer(h_centi, t, height);
+    cout << magicHammer(h_centi, t, height);
     return 0;
 }
--- 09월 30일 - 우선순위 큐/23757.cpp
+++ 09월 30일 - 우선순위 큐/23757.cpp
@@ -8,13 +8,10 @@ typedef priority_queue<int> pq;
 bool presentGifts(pq c, int m, vector<int> w) {
     for(int i = 0; i < m; i++) {
         int gift = c.top(); c.pop(); // (현재 선물이 가장 많이 담겨있는 상자의 선물 수)
-        // 1. 선물을 가져가는 경우
-        if(gift >= w[i]) {
-            c.push(gift-w[i]); // 원하는 양 가져가기
-            continue;
-        }
-        // 2. 선물을 가져가지 못하는 경우
-        return false;
+        // 1. 선물을 가져가지 못하는 경우
+        if(gift < w[i]) return false;
+        // 2. 선물을 가져가는 경우
+        c.push(gift-w[i]); // 원하는 양 가져가기
     }
     return true;
 }
